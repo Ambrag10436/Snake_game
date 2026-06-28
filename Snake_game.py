a38d250d586a946ea92265d8169a3b30ac59061b
Ambrag10436 <Ambrag@mail.ru>
2026-06-28 10:34:59 +0200

    food generation

--- Snake_game.py
+++ Snake_game.py
@@ -1,9 +1,17 @@
 import tkinter as tk
+import random
+
+
 
 #размеры окна
 
 WIDTH = 400
 HEIGHT = 400
+DIRECTIONS = ["Up", "Down", "Left", "Right"]
+CELL_Size = 10
+DELAY = 100 #задержка между движениями змеи
+
+
 
 # создаем главное окно
 root = tk.Tk()
@@ -21,13 +29,17 @@ canvas.pack()
 
 snake = [(100, 100), (90, 100), (80, 100)]
 direction = "Right"
-DIRECTIONS = ["Up", "Down", "Left", "Right"]
-
-food = None
 score = 0
-
 game_over = False
 
+
+def create_food():
+    while True:
+        x = random.randint(0, (WIDTH - CELL_SIZE) // CELL_SIZE) * CELL_Size
+        y = random.randint(0, (HEIGHT - CELL_SIZE) // CELL_SIZE) * CELL_Size
+        if (x, y) not in snake: # проверка не находится ли еда внутри змеи
+            return (x, y)
+food = create_food
 
 
 #Запуск главного цикла программы
