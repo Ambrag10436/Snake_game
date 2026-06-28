6f41755520e02923e1c3e800dad2c92b754e6a33
Ambrag10436 <Ambrag@mail.ru>
2026-06-28 13:56:10 +0200

    draw food

--- Snake_game.py
+++ Snake_game.py
@@ -41,6 +41,23 @@ def create_food():
             return (x, y)
 food = create_food
 
+def draw_food():
+    canvas.create_rectangle(
+        food[0], food[1],
+        food[0] + CELL_SIZE,
+        food[1] + CELL_SIZE,
+        fill="red",
+    )
+
+def game_loop():
+    global snake, food, score
+
+    canvas.delete("all")
+    draw_food()
+    root.after(DELAY, game_loop)
+
+draw_food()
+root.after(DELAY, game_loop)
 
 #Запуск главного цикла программы
 root.mainloop()
